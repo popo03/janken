fccd6a35bd3ea25eb00ae7d466e6801a00d4ee53
popo03 <83389638+popo03@users.noreply.github.com>
2021-08-18 15:20:18 +0900

    top画面の作成

--- index.php
+++ index.php
@@ -6,5 +6,31 @@
   <link ref="stylesheet" href="style.css">
 </head>
 <body>
+<div id="wrapper">
+  <header>
+    <div>PHPで作った、じゃんけんプログラム</div>
+  </header>
+  <main>
+    <h1>じゃんけん</h1>
+    出す手を選んで勝負して下さい。
+    <div>
+      <form action="battle.php" method="post">
+        <label>
+          <input type="radio" title="playerHand" name="playerHand" value="グー" checked>グー
+        </label>
+        <label>
+          <input type="radio" title="playerHand" name="playerHand" value="チョキ">チョキ
+        </label>
+        <label>
+          <input type="radio" title="playerHand" name="playerHand" value="パー">パー
+        </label>
+        <button type="submit">勝負する！</button>
+      </form>
+    </div>
+  </main>
+  <footer>
+    <small>&copy;php</small>
+  </footer>
+</div>
 </body>
 </html>
